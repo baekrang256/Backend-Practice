84e90f53ca87e39f84834655b0db25e38bc206f6
Seiyeon Cho <baekrang256@gmail.com>
2024-01-05 21:49:11 +0900

    Accessing Relational Data using JDBC with Spring complete

---
+++ relationaldataaccessJDBC/src/test/java/org/example/relationaldataaccessjdbc/RelationaldataaccessJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.relationaldataaccessjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationaldataaccessJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
