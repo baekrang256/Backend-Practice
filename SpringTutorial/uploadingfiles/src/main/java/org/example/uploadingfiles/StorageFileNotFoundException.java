c5a5e1a2737bd80287b1d77daa222fd1c013bd0a
Seiyeon Cho <baekrang256@gmail.com>
2024-01-09 01:02:55 +0900

    Uploading File finished

---
+++ SpringTutorial/uploadingfiles/src/main/java/org/example/uploadingfiles/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package org.example.uploadingfiles;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ SpringTutorial/uploadingfiles/src/main/java/org/example/uploadingfiles/UploadingfilesApplication.java
@@ -0,0 +1,24 @@
+package org.example.uploadingfiles;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
+public class UploadingfilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingfilesApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+}
