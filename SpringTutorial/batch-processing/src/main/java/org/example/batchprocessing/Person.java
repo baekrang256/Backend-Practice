dc61588b1a98f9ee11d19721b5dc8370d87fee7a
Seiyeon Cho <baekrang256@gmail.com>
2024-01-10 20:34:27 +0900

    Batch Processing Complete

---
+++ SpringTutorial/batch-processing/src/main/java/org/example/batchprocessing/Person.java
@@ -0,0 +1,5 @@
+package org.example.batchprocessing;
+
+public record Person(String firstName, String lastName) {
+
+}
---
+++ SpringTutorial/messaging-jms/src/test/java/org/example/messagingjms/MessagingJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.messagingjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
