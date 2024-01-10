71a4250a82e5f036ecec584820a1c5eb16477227
Seiyeon Cho <baekrang256@gmail.com>
2024-01-11 01:56:44 +0900

    managing transactions complete

---
+++ SpringTutorial/accessing-data-gemfire/src/main/java/org/example/accessingdatagemfire/AccessingDataGemfireApplication.java
@@ -0,0 +1,13 @@
+package org.example.accessingdatagemfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessingDataGemfireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessingDataGemfireApplication.class, args);
+    }
+
+}
---
+++ SpringTutorial/accessing-data-gemfire/src/test/java/org/example/accessingdatagemfire/AccessingDataGemfireApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.accessingdatagemfire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessingDataGemfireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
