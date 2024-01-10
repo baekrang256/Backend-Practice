97ae3a85612189bfe68b60ca43c4306e0d67aee1
Seiyeon Cho <baekrang256@gmail.com>
2024-01-11 01:20:42 +0900

    rest hateoas complete

---
+++ SpringTutorial/rest-hateoas/src/test/java/org/example/resthateoas/RestHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.resthateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestHateoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ SpringTutorial/securing-web/src/test/java/org/example/securingweb/SecuringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.securingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
